cf8366acff6ecf4f7524b1f6b01bbc454769b031
brands <brandonlee.swanepoel@gmail.com>
2020-08-17 16:25:16 +0200

    Added write to file function

--- Main.java
+++ Main.java
@@ -1,23 +1,34 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
-import ErrorChecking.ErrorChecking;
 import aircraft.AircraftFactory;
 import aircraft.Flyable;
+import errorChecking.ErrorChecking;
 import tower.WeatherTower;
 
 public class Main {
 
   public static void main(String[] args) {
+    PrintStream fileStream = null;
+    try {
+      fileStream = new PrintStream("simulation.txt");
+      System.setOut(fileStream);
+    } catch (FileNotFoundException e1) {
+      System.out.println("There was an error while trying to write to simulation.txt");
+      e1.printStackTrace();
+    }
+
     int simulations = 0;
     WeatherTower weatherTower = new WeatherTower();
     try {
-      File file = new File("sources.txt");
+      File file = new File(args[0]);
       if (file != null) {
         Scanner fileReader = new Scanner(file);
         simulations = ErrorChecking.checkIfSimulationIsNumber(fileReader);
-        
+        ErrorChecking.checkIfSimulationIsNotNegativeOrZero(simulations, fileReader);
+
         while (fileReader.hasNextLine()) {
           String data = fileReader.nextLine();
           if (data != null) {
@@ -32,7 +43,6 @@ public class Main {
             if (aircraft != null) {
               aircraft.registerTower(weatherTower);
             }
-
           }
         }
         for (int i = 1; i <= simulations; i++) {
@@ -42,6 +52,7 @@ public class Main {
           weatherTower.changeWeather();
         }
         fileReader.close();
+        fileStream.close();
       }
     } catch (FileNotFoundException e) {
       System.out.println("An error occurred.");
